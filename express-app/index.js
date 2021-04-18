f6dda4119f7248fbfbd10649ca2edd0a294d38d7
Shashank Korthiwada <skorthiwada@manh.com>
2021-04-18 15:26:35 +0530

    refactor: Changed Welcome Message

--- express-app/index.js
+++ express-app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, resp) => {
-  resp.send("Hello World!!");
+  resp.send("Welcome this is a Express App!!");
 });
 
 app.listen(port, () => {
